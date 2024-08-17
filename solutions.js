6a84df65051cad726b834f837137a487cac7df74
Rafael Salcedo <rafaelsava@unisabana.edu.co>
2024-08-17 14:07:42 -0500

    completed1

--- solutions.js
+++ solutions.js
@@ -26,48 +26,47 @@ function factorial(n) {
 // Find the Largest Number
 function findLargest(arr) {
   let Largest = arr[0];
-  for (let number in arr){
-    if (typeof number != 'number'){
-      console.log("Los datos de entrada no son adecuados");
-      break;
-    }
-    if (number>Largest){
-      Largest = number;
-    }
-    
+  for (let num of arr){
+      if (typeof num != 'number'){
+          console.log("Los datos de entrada no son adecuados");
+          break;
+      }
+      if (num>Largest){
+          Largest = num;
+      }
   }
+  return Largest;
 }
-
 // Count Vowels in a String
 function countVowels(str) {
   const vowels = ['a','e','i','o','u'];
   let numVowels = 0;
-  for (const char in str){
-    if (typeof char != 'string'){
+  for (const char of str){
+  if (typeof char != 'string'){
       console.log("Los datos de entrada no son adecuados");
       break;
-    }
-    else if (vowels.includes(char.toLowerCase())){
-        numVowels++;
-    }
+  }
+  else if (vowels.includes(char.toLowerCase())){
+      numVowels++;
+  }
   }
   return numVowels;
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  if(typeof n != 'number'){
-    console.log("Los datos de entrada no son adecuados");
+  if (typeof n !== 'number' || n < 2) {
+  console.log("Los datos de entrada no son adecuados");
+  return false;
   }
-  else{
-    for(let i;i<n;i++){
-      if (n%i==0){
-        return false
-      }
-      return true
-    }
+  if (n === 2) return true; 
+  for (let i = 2; i < n; i++) {
+  if (n % i === 0) return false;
   }
+
+  return true;
 }
+
 
 module.exports = {
   sum,
